98d6bf5fd26615ad046226a224df5d124d1b4533
graciochiziane <graciochiziane@gmail.com>
2026-05-11 14:44:39 +0200

    feat: add PostHog provider to app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { PostHogProvider } from 'posthog-js/react';
+import { posthogClient } from './lib/posthog';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const AppWithAnalytics = posthogClient
+  ? () => (
+      <PostHogProvider client={posthogClient}>
+        <App />
+    </PostHogProvider>
+  )
+  : () => <App />;
+
+createRoot(document.getElementById("root")!).render(<AppWithAnalytics />);
